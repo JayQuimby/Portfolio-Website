ab515264315c7a7e5e452dbefe5e26eca3b9d5df
Jack Quimby <jack.quimby54@gmail.com>
2023-11-26 18:18:56 -0500

    Portfolio Website

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+app = Flask(__name__, template_folder='templates')
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/education")
+def education():
+    return render_template("education.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0', port=4000)
